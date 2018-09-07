9f1949eb9548ec731b12dff767704051a42f96f1
Evgeny Ladyko <evgeniy.ladyko@gmail.com>
2018-09-07 17:12:59 +0400

    Add project files.

---
+++ snake/Field.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+	class Field : DrawableGameComponent
+	{
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public Field(Game game, int width, int height) : base(game)
+		{
+			Width = width;
+			Height = height;
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+		}
+	}
+}
