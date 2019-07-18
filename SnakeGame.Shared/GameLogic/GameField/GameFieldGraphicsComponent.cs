42b71314941a4480cfc5806d789208c010438233
Evgeniy Ladyko <evgeniy.ladyko@gmail.com>
2019-07-18 18:17:02 +0400

    variable rename;

--- SnakeGame.Shared/GameLogic/GameField/GameFieldGraphicsComponent.cs
+++ SnakeGame.Shared/GameLogic/GameField/GameFieldGraphicsComponent.cs
@@ -18,26 +18,26 @@ namespace SnakeGame.Shared.GameLogic.GameField
     public class GameFieldGraphicsComponent : IGraphics2DComponent
     {
         private readonly IGameField _gameField;
-        private readonly IGraphicsSystem _renderingCore;
+        private readonly IGraphicsSystem _graphicsSystem;
         private readonly IGameSettings _gameSettings;
-        private readonly IGraphicsSettings _renderSettings;
+        private readonly IGraphicsSettings _graphicsSettings;
 
-        public GameFieldGraphicsComponent(IGameField gameField, IGraphicsSettings renderSettings, IGraphicsSystem renderingCore, IGameSettings gameSettings)
+        public GameFieldGraphicsComponent(IGameField gameField, IGraphicsSettings graphicsSettings, IGraphicsSystem graphicsSystem, IGameSettings gameSettings)
         {
             _gameField = gameField ?? throw new ArgumentNullException(nameof(gameField));
-            _renderSettings = renderSettings ?? throw new ArgumentNullException(nameof(renderSettings));
-            _renderingCore = renderingCore ?? throw new ArgumentNullException(nameof(renderingCore));
+            _graphicsSettings = graphicsSettings ?? throw new ArgumentNullException(nameof(graphicsSettings));
+            _graphicsSystem = graphicsSystem ?? throw new ArgumentNullException(nameof(graphicsSystem));
             _gameSettings = gameSettings ?? throw new ArgumentNullException(nameof(gameSettings));
         }
 
         public void Draw(GameTime gameTime)
         {
-            if (_renderSettings.IsRenderingEnabled)
+            if (_graphicsSettings.IsRenderingEnabled)
             {
                 Rendering();
             }
 
-            if (_renderSettings.IsDebugRenderingEnabled)
+            if (_graphicsSettings.IsDebugRenderingEnabled)
             {
                 DebugRendering();
             }
@@ -76,12 +76,12 @@ namespace SnakeGame.Shared.GameLogic.GameField
         {
             for (int x = 0; x <= _gameField.Bounds.Width; x += _gameSettings.TileSize)
             {
-                _renderingCore.SpriteBatch.DrawLine(new Vector2(x, 0), new Vector2(x, _gameField.Bounds.Height), Color.Black);
+                _graphicsSystem.SpriteBatch.DrawLine(new Vector2(x, 0), new Vector2(x, _gameField.Bounds.Height), Color.Black);
             }
 
             for (int y = 0; y <= _gameField.Bounds.Height; y += _gameSettings.TileSize)
             {
-                _renderingCore.SpriteBatch.DrawLine(new Vector2(0, y), new Vector2(_gameField.Bounds.Width, y), Color.Black);
+                _graphicsSystem.SpriteBatch.DrawLine(new Vector2(0, y), new Vector2(_gameField.Bounds.Width, y), Color.Black);
             }
         }
 
@@ -96,8 +96,8 @@ namespace SnakeGame.Shared.GameLogic.GameField
                 {
                     var cell = _gameField.Cells[x, y];
 
-                    _renderingCore.SpriteBatch.DrawString(
-                        spriteFont: _renderingCore.DebugSpriteFont,
+                    _graphicsSystem.SpriteBatch.DrawString(
+                        spriteFont: _graphicsSystem.DebugSpriteFont,
                         text: $"{cell.Column};{cell.Row}",
                         position: cell.Position,
                         color: Color.Black,
@@ -112,10 +112,10 @@ namespace SnakeGame.Shared.GameLogic.GameField
 
         private void DrawGrass(ICell cell)
         {
-            _renderingCore.SpriteBatch.Draw(
-                texture: _renderingCore.TextureManager.TextureRegions["Grass"].Texture,
+            _graphicsSystem.SpriteBatch.Draw(
+                texture: _graphicsSystem.TextureManager.TextureRegions["Grass"].Texture,
                 destinationRectangle: cell.Bounds,
-                sourceRectangle: _renderingCore.TextureManager.TextureRegions["Grass"].Bounds,
+                sourceRectangle: _graphicsSystem.TextureManager.TextureRegions["Grass"].Bounds,
                 color: Color.White,
                 rotation: 0,
                 origin: Vector2.Zero,
@@ -125,10 +125,10 @@ namespace SnakeGame.Shared.GameLogic.GameField
 
         private void DrawTree(ICell cell)
         {
-            _renderingCore.SpriteBatch.Draw(
-                texture: _renderingCore.TextureManager.TextureRegions["Tree"].Texture,
+            _graphicsSystem.SpriteBatch.Draw(
+                texture: _graphicsSystem.TextureManager.TextureRegions["Tree"].Texture,
                 destinationRectangle: cell.Bounds,
-                sourceRectangle: _renderingCore.TextureManager.TextureRegions["Tree"].Bounds,
+                sourceRectangle: _graphicsSystem.TextureManager.TextureRegions["Tree"].Bounds,
                 color: Color.White,
                 rotation: 0,
                 origin: Vector2.Zero,
