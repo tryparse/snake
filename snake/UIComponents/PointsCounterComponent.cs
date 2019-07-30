dc88c011e5e53bcfb3c5bfcea9773170441a8e80
Evgeniy Ladyko <evgeniy.ladyko@gmail.com>
2019-07-30 11:34:18 +0400

    Points counter draw with picture;

--- snake/UIComponents/PointsCounterComponent.cs
+++ snake/UIComponents/PointsCounterComponent.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.TextureAtlases;
+using SnakeGame.Shared.Common;
 
 namespace snake.UIComponents
 {
@@ -14,17 +18,27 @@ namespace snake.UIComponents
         private readonly IGraphicsSystem _graphicsSystem;
         private readonly IGamePoints _gamePoints;
         private readonly Vector2 _position;
+        private readonly TextureRegion2D _foodTexture2D;
 
         public PointsCounterComponent(Game game, Vector2 position, IGraphicsSystem graphicsSystem, IGamePoints gamePoints) : base(game)
         {
             _position = position;
             _graphicsSystem = graphicsSystem ?? throw new ArgumentNullException(nameof(graphicsSystem));
             this._gamePoints = gamePoints;
+
+            _foodTexture2D = graphicsSystem.TextureManager.TextureRegions.GetRegion("Fruit");
         }
 
         public override void Draw(GameTime gameTime)
         {
-            _graphicsSystem.SpriteBatch.DrawString(_graphicsSystem.SpriteFont, $"Points: {_gamePoints.Points}", _position, Color.Green);
+            var destination = new Rectangle((int)_position.X, (int) _position.Y, 25, 25);
+
+            var size = new Size2(25, 25);
+            var scale = new Vector2(size.Width / (float)_foodTexture2D.Width, size.Height / (float)_foodTexture2D.Height);
+            var textPosition = new Vector2(_position.X + size.Width, _position.Y);
+
+            _graphicsSystem.SpriteBatch.Draw(_foodTexture2D.Texture, _position, _foodTexture2D.Bounds, Color.White, 0, Vector2.Zero, scale, SpriteEffects.None, LayerDepths.Debug);
+            _graphicsSystem.SpriteBatch.DrawString(_graphicsSystem.SpriteFont, $" x {_gamePoints.Points}", textPosition, Color.Green);
         }
     }
 }
