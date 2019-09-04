fec0b0bd10ea621cc9c21b49e06d1768a380ff68
Evgeniy Ladyko <evgeniy.ladyko@gmail.com>
2019-09-04 18:45:16 +0400

    Added GameOverScene class;

---
+++ SnakeGame.Shared/SceneManagement/GameOverScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SnakeGame.Shared.SceneManagement
+{
+    class GameOverScene : IScene
+    {
+
+
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
